60bc0fa206b2b0bf63a86078b64364d6e45c8cec
andrewlod <andremacwlod@gmail.com>
2022-03-04 21:51:11 -0300

    Created Django structure

---
+++ scripts/manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import argparse
+
+sys.path.append(os.getcwd())
+
+def main(args):
+    """Run administrative tasks."""
+    env = 'project.settings.production' if args.env == 'production' else 'project.settings.development'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', env)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("env", help="Enviroment: development (default) or production")
+    args = parser.parse_args()
+    main(args)
