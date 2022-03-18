f2906bdd399b903618e9d39b375b40dbfa524b6c
andrewlod <andremacwlod@gmail.com>
2022-03-18 08:46:14 -0300

    Download fixes and model predict

--- project/apps/api/util/StorageHandler.py
+++ project/apps/api/util/StorageHandler.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 import os
+import zipfile
 from .. import constants
 from django.core.files.storage import default_storage
 
@@ -18,15 +19,36 @@ def store_file(file, storage_type, file_id=None):
 
     return file_id, storage_type_id
 
-def download_file(file_id):
+def download_and_extract(file_id):
     data = default_storage.open(file_id).read()
     
-    directory = f"temp/{file_id}/"
+    directory = f"temp/{file_id}"
     os.makedirs(directory, exist_ok=True)
-    path = directory + "saved_model.pb"
+    path = directory + ".zip"
 
     with open(path, 'wb+') as file:
         file.write(data)
         file.close()
     
+    with zipfile.ZipFile(path, 'r') as zipped:
+        zipped.extractall(directory)
+    
+    os.remove(path)
+    
     return directory
+
+
+def download_file(file_id):
+    data = default_storage.open(file_id).read()
+    
+    path = f"temp/{file_id}"
+
+    with open(path, 'wb+') as file:
+        file.write(data)
+        file.close()
+    
+    return path
+
+
+def read_file(file_id):
+    return default_storage.open(file_id).read()
--- project/apps/api/views.py
+++ project/apps/api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+import shutil
 from project.apps.api.util import StorageHandler
 from .models import TFModel
 
@@ -15,23 +16,20 @@ def get_model_response(request, model_id):
     except:
         return HttpResponse("Model not found!")
     
-    model_dir = StorageHandler.download_file(tf_model.model_storage.storage_id)
+    model_dir = StorageHandler.download_and_extract(tf_model.model_storage.storage_id)
     
     model = tf.keras.models.load_model(model_dir)
-    print(list(model.signatures.keys()))
-
-    """
-    label_map = None
-    with open("aiy_food_V1_labelmap.txt", "r", encoding="cp850") as file:
-        label_map = file.read().split(",")
+    label_map_file = StorageHandler.read_file(tf_model.label_map_storage.storage_id).decode("utf-8")
+    label_map = label_map_file.split(",")
     
     image = tf.keras.preprocessing.image.load_img("test.jpg")
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([tf.image.resize(input_arr, (192,192)).numpy()])
+    input_arr = np.array([tf.image.resize(input_arr, (300,300)).numpy()])
 
     predictions = model.predict(input_arr)
     max_index = np.argmax(predictions)
 
+    shutil.rmtree(model_dir)
+
     return HttpResponse(label_map[max_index])
-    """
-    return HttpResponse("test")
+
