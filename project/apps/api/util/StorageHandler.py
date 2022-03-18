70fef02a0b51dffca4bf2e7f2d3bc357780ca2a0
andrewlod <andremacwlod@gmail.com>
2022-03-17 22:00:55 -0300

    Download model

--- project/apps/api/util/StorageHandler.py
+++ project/apps/api/util/StorageHandler.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+import os
 from .. import constants
 from django.core.files.storage import default_storage
 
@@ -16,3 +17,16 @@ def store_file(file, storage_type, file_id=None):
         return False
 
     return file_id, storage_type_id
+
+def download_file(file_id):
+    data = default_storage.open(file_id).read()
+    
+    directory = f"temp/{file_id}/"
+    os.makedirs(directory, exist_ok=True)
+    path = directory + "saved_model.pb"
+
+    with open(path, 'wb+') as file:
+        file.write(data)
+        file.close()
+    
+    return directory
--- project/apps/api/views.py
+++ project/apps/api/views.py
@@ -3,11 +3,24 @@ from django.http import HttpResponse
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+from project.apps.api.util import StorageHandler
+from .models import TFModel
 
-
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def get_model_response(request, model_id):
-    model = tf.keras.Sequential([hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/food_V1/1')])
+    tf_model = None
+    try:
+        tf_model = TFModel.objects.get(id=model_id)
+    except:
+        return HttpResponse("Model not found!")
+    
+    model_dir = StorageHandler.download_file(tf_model.model_storage.storage_id)
+    
+    model = tf.keras.models.load_model(model_dir)
+    print(list(model.signatures.keys()))
 
+    """
     label_map = None
     with open("aiy_food_V1_labelmap.txt", "r", encoding="cp850") as file:
         label_map = file.read().split(",")
@@ -20,3 +33,5 @@ def get_model_response(request, model_id):
     max_index = np.argmax(predictions)
 
     return HttpResponse(label_map[max_index])
+    """
+    return HttpResponse("test")
